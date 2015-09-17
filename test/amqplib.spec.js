42cc210a93724b27cc6e10ee7dd98f8b0284730f
Bernard Labno <bernard.labno@pjwstk.edu.pl>
2015-09-17 09:23:17 +0200

    when there are 2 consumers, one always rejecting and another always accepting

--- test/amqplib.spec.js
+++ test/amqplib.spec.js
@@ -51,40 +51,39 @@ describe('amqplib', function () {
         }));
     }
 
+    function createPool() {
+        var pool = { channels: [], connections: []};
+        pool.channelClosed = function (channel) {
+            var indexOf = pool.channels.indexOf(channel);
+            if (indexOf > -1) {
+                pool.channels.splice(indexOf, 1);
+            }
+        };
+        return pool;
+    }
+
+    function cleanupPool(pool) {
+        return Promise.all(_.map(pool.channels, function (channel) {
+                return channel && channel.close();
+            })).then(function () {
+                return Promise.all(_.map(pool.connections, function (connection) {
+                    return connection && connection.close();
+                }));
+            });
+    }
+
     describe('ack', function () {
         var consumerChannel;
         var pool;
 
         beforeEach(function () {
-            pool = { channels: [], connections: []};
-            pool.channelClosed = function (channel) {
-                var indexOf = pool.channels.indexOf(channel);
-                if (indexOf > -1) {
-                    pool.channels.splice(indexOf, 1);
-                }
-            };
+            pool = createPool();
             return purge(pool).then(connectAsPublisher).then(function (channel) {
                 return send(channel, 'abc', 'def');
             });
         });
         afterEach(function () {
-            _.forEach(pool.channels, function (item, key) {
-                _.forEach(pool.channels, function (item2, key2) {
-                    if (key === key2) {
-                        return;
-                    }
-                    if (item === item2) {
-                        console.log('found duplicate');
-                    }
-                });
-            });
-            return Promise.all(_.map(pool.channels, function (channel) {
-                    return channel && channel.close();
-                })).then(function () {
-                    return Promise.all(_.map(pool.connections, function (connection) {
-                        return connection && connection.close();
-                    }));
-                });
+            return cleanupPool(pool);
         });
         describe('when first message is NOT acknowledged', function () {
             var consumerSpy;
@@ -126,14 +125,74 @@ describe('amqplib', function () {
         });
     });
     describe('nack', function () {
+
+        var pool;
+        beforeEach(function () {
+            pool = createPool();
+        });
+        afterEach(function () {
+            return cleanupPool(pool);
+        });
         describe('when there are 2 consumers, one always rejecting and another always accepting', function () {
+            var consumerSpyA;
+            var consumerSpyB;
+            beforeEach(function () {
+                var consumerChannelA;
+                var consumerChannelB;
+                consumerSpyA = sinon.spy(function (msg) {
+                    consumerChannelA.nack(msg);
+                });
+                consumerSpyB = sinon.spy(function (msg) {
+                    consumerChannelB.ack(msg);
+                });
+                return purge(pool).then(connectAsPublisher).then(function (channel) {
+                    return send(channel, 'a', 'b', 'c', 'd', 'e');
+                }).then(function () {
+                        return  connect(pool);
+                    }).then(function (channel) {
+                        consumerChannelA = channel;
+                        return consumerChannelA.consume(queueName, consumerSpyA);
+                    }).then(function () {
+                        return  connect(pool);
+                    }).then(function (channel) {
+                        consumerChannelB = channel;
+                        return consumerChannelB.consume(queueName, consumerSpyB);
+                    }).then(function () {
+                        return Promise.delay(100);
+                    });
+            });
+
             it('should finally get all messages consumed', function () {
-                throw new Error('Not implemented yet');
+                expect(consumerSpyA.callCount).to.be.above(0);
+                expect(consumerSpyB).to.have.been.callCount(5);
             });
         });
         describe('when message is rejected for the first time', function () {
+            var consumerSpy;
+            beforeEach(function () {
+                var consumerChannel;
+                var callCount = 0;
+                consumerSpy = sinon.spy(function (msg) {
+                    if (callCount) {
+                        consumerChannel.ack(msg);
+                    } else {
+                        consumerChannel.nack(msg);
+                    }
+                    callCount++;
+                });
+                return purge(pool).then(connectAsPublisher).then(function (channel) {
+                    return send(channel, 'abc');
+                }).then(function () {
+                        return  connect(pool).then(function (channel) {
+                            consumerChannel = channel;
+                            return consumerChannel.consume(queueName, consumerSpy);
+                        });
+                    }).then(function () {
+                        return Promise.delay(100);
+                    });
+            });
             it('should be redelivered to the same (only one) consumer', function () {
-                throw new Error('Not implemented yet');
+                expect(consumerSpy).to.have.been.callCount(2);
             });
         });
     });
